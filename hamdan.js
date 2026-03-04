41731806dc1fc7d13c0c1b0ac752b4d23c4bc816
Hamdan Khan <143438295+hamdankhann1@users.noreply.github.com>
2026-03-04 15:48:00 +0500

    added save functionality

---
+++ hamdan.js
@@ -0,0 +1,18 @@
+function saveChecklist() {
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    const state = {};
+    checkboxes.forEach(cb => {
+        state[cb.id] = cb.checked;
+    });
+    localStorage.setItem('checklistState', JSON.stringify(state));
+}
+
+// Load state on page load
+window.onload = () => {
+    const savedState = JSON.parse(localStorage.getItem('checklistState'));
+    if (savedState) {
+        Object.keys(savedState).forEach(id => {
+            document.getElementById(id).checked = savedState[id];
+        });
+    }
+};
